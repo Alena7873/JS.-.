f41600cdb5347374980515701a3d6d7baec144cb
Alena <115954741+Alena7873@users.noreply.github.com>
2023-02-13 00:27:45 +0200

    урок 2

--- script.js
+++ script.js
@@ -1,23 +1,105 @@
-// alert виводит сообщения моментально
-//alert('Hello!')
-//confirm('Ви вивчаєте  JS');
+// переменная коробка в которой храняться даннние
+// var c = 15;
+// let a = 'seven';
+// a = 7;
+// const b = 8;
+// строка
 
-// модально окно с ответом по умолчанию
-//prompt(" Какой язик ви учите  ", "пока не в курсе");
+// const str1 = 'Greeting \Привітання'
+// const str2 = "Text"
+// const str3 = `Reverse \Зворотний`
+// const str4 = 'I\'m Ok'
+// const str5 = "Саша \nсказал: \"Привет\"" // \-слеш \n- синтаксис розрив строки ентер
+// const str6 = `I'm say ${str1}` // шаблонная строка
 
-//обратились к елементу
-const skill = document.getElementById('skill');
-const isLove = document.getElementById('isLove');
-const string = document.getElementById('string')
+// // typeof узнайом тип переменной
+// console.log(str1,)
+// console.log(str2)
+// console.log(str3)
+// console.log(str4)
+// console.log(str5)
+// console.log(str6)
 
-//const skillText = prompt(" Какой язик ви учите  ", "пока не в курсе");
-//const isLoveValue = confirm('Вы любите изучаемий язык?');
+//пример 2  NUMBER + - * / ** - степень
 
-//console.log(isLoveValue);
+// const num1 = 75
+// const num2 = 79 ** 3
+// console.log(typeof "5")
+// const rem = 7 + 8 + "5" // cтрочки склеиваються  155
+// const rem1 = 8 * "a" // NAN не число
+// const inf = 17 / 0  // Infinity  БЕСКОНЕЧНОСТЬ
+// console.log(rem)
+// console.log(rem1)
+// console.log(inf)
+// // alert(num1)
+// // alert(num2)
+// oт -(2**53 -1) до (2**53 -1)
+//console.log(999999999999999999999999) // округления происходить
 
-//skill.innerText = skillText;
-//isLove.innerText = isloveValue;
-//string.innerHTML = "str"
-const example = document.getElementById('example')
-console.log(example.innerHTML)
-console.log(example.innerText)
+// пример 3 BIGINT
+
+//const bigint = 103123123n  // какое число написали такое и покажет вконце буква n
+
+//  пример 4 BOOLEAN
+//true  правда  / false не правда
+// > больше < меньше >= <=  >== <==  == равно === строгое равно
+
+//const bool = "a" > "AAA"
+//const bool = "a" > "8"
+//console.log(bool)
+
+// пример 5 NULL  typeof null повертає object, це офіційно визнана помилка.
+//let empty = null
+//console.log(empty,typeof empty)
+
+// пример 6 UNDEFINED
+
+//let box = undefined
+//console.log(box, typeof box)
+
+// пример 7 SYMBOL  УНИКАЛЬНАЯ СИМВОЛ
+//const uniq1 = Symbol('id')
+//const uniq2 = Symbol('id')
+//console.log(uniq1 == uniq2)
+
+// пример 8 OBJECT  набор пар ключ значения
+// const obj = {
+//     name: "Sasha",
+//     age: 19,
+//     isHappy: true,
+// }
+//console.log(obj.name)
+//console.log(obj['name']) // 2
+
+// obj.job = "Google"
+// const array = ["Аня", 18, false]
+// array.job = "Facebook"
+// console.log(array[0]) // масив[]
+// //console.log(obj)
+
+const asa = "342" + 12
+console.log(asa)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
