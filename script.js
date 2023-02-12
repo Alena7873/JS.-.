5a82040becf38d30d5adef8cdb071b847d86d730
Alena <115954741+Alena7873@users.noreply.github.com>
2023-02-12 19:46:17 +0200

    урок 1

---
+++ script.js
@@ -0,0 +1,23 @@
+// alert виводит сообщения моментально
+//alert('Hello!')
+//confirm('Ви вивчаєте  JS');
+
+// модально окно с ответом по умолчанию
+//prompt(" Какой язик ви учите  ", "пока не в курсе");
+
+//обратились к елементу
+const skill = document.getElementById('skill');
+const isLove = document.getElementById('isLove');
+const string = document.getElementById('string')
+
+//const skillText = prompt(" Какой язик ви учите  ", "пока не в курсе");
+//const isLoveValue = confirm('Вы любите изучаемий язык?');
+
+//console.log(isLoveValue);
+
+//skill.innerText = skillText;
+//isLove.innerText = isloveValue;
+//string.innerHTML = "str"
+const example = document.getElementById('example')
+console.log(example.innerHTML)
+console.log(example.innerText)
